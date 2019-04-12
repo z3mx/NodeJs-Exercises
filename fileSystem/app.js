45d8d9c6077afa7051932618c569dc40e3a321fa
luis antonio <lantonio@localhost.localdomain>
2019-04-11 23:23:05 -0500

    addin fs functions exercises

---
+++ fileSystem/app.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+/***************************************************************/
+//Creating file
+let content = "\n adding second line to file with NodeJS!"
+
+fs.appendFile("data.txt", content, function (error) {
+    if (error) {
+        console.log("Error creating file!");
+    } else {
+        console.log("File created");
+    }
+});
+
+/****************************Sync Function***********************/
+    console.log("init");
+
+    fs.readFile('data.txt', 'utf-8', (error, data) => {
+        if (error) {
+            console.log(`Error ${error}`);
+        } else {
+            console.log(data);
+        }
+    });
+    console.log("endprocess");
+
+/****************************Async Function***********************/
+console.log("init process 2");
+
+let data = fs.readFileSync('data.txt', 'utf-8');
+console.log(data);
+console.log("endprocess 2");
+
+/****************************delete file Function***********************/
+
+/*fs.unlink('data.txt', (error) => {
+    if (error) throw error;
+    console.log("File Deleted!");
+
+});
+/****************************delete file Function***********************/
